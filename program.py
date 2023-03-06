7f3b075db652f09093d403b2f4cb1d4ba3bcb0fa
AidenVu1 <100857445+AidenVu1@users.noreply.github.com>
2023-03-06 15:46:11 +0000

    commit

--- program.py
+++ program.py
@@ -2,4 +2,7 @@
 # Task:  Use the function myFunction to output a simple "Hello World!" statement
 
 def myFunction():
-  # My Function
+    mytext = "Hello World"
+    return mytext
+
+print(myFunction())
